09160ce907a489b11e7ef09576792335c9e7160e
روجر <104784913+rogerpq@users.noreply.github.com>
2022-09-10 01:04:25 +0300

    Update __main__.py

--- jepthon/__main__.py
+++ jepthon/__main__.py
@@ -49,10 +49,10 @@ async def startup_process():
     await load_plugins("plugins")
     await load_plugins("assistant")
     print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
-    print("⌯︙بـوت ماتـركس يعـمل بـنجاح ")
+    print("⌯︙بـوت ريبثون يعـمل بـنجاح ")
     print(
         f"يجـب تفـعيل وضع الأنلايـن ثم أرسـل {cmdhr}فحص لـرؤيـة اذا كـان البوت شـغال\
-        \nللمسـاعدة تواصـل  https://t.me/ZBBBBB"
+        \nللمسـاعدة تواصـل  https://t.me/Repthon_support"
     )
     print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
     await verifyLoggerGroup()
