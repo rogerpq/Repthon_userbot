ea47ef7e0f36fb03c8ca20a109efaf847ba2b1a8
روجر <104784913+rogerpq@users.noreply.github.com>
2022-09-12 09:13:59 +0300

    Update __init__.py

--- jepthon/__init__.py
+++ jepthon/__init__.py
@@ -10,7 +10,7 @@ from .sql_helper.globals import addgvar, delgvar, gvarstatus
 __version__ = "1.0.0"
 __license__ = "كـتابة وتـعديل فريـق ريبثون"
 __author__ = "ريبثون <https://T.ME/Repthon>"
-__copyright__ = "JEPTHON TEAM (C) 2020 - 2021  " + __author__
+__copyright__ = "Repthon TEAM (C) 2020 - 2021  " + __author__
 
 jepiq.version = __version__
 jepiq.tgbot.version = __version__
@@ -22,7 +22,7 @@ JMVERSION = "5.2.0"
 
 
 if Config.UPSTREAM_REPO == "Rep":
-    UPSTREAM_REPO_URL = "https://github.com/rogerpq/Repthon_userboot"
+    UPSTREAM_REPO_URL = "https://github.com/rogerpq/Repthon-"
 else:
     UPSTREAM_REPO_URL = Config.UPSTREAM_REPO
 
