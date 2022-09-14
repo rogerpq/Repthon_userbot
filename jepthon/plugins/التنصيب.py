cce25d6376c829292aec12ef35e0d6c786b2f17c
روجر <104784913+rogerpq@users.noreply.github.com>
2022-09-14 23:30:26 +0300

    Update التنصيب.py

--- jepthon/plugins/التنصيب.py
+++ jepthon/plugins/التنصيب.py
@@ -6,4 +6,4 @@ import asyncio
 @borg.on(admin_cmd("تنصيب السورس"))
 async def _(event):
      if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
-        await event.edit("⌁︙**اهـلا بك فـي تنصيب السورس** \n⌁︙رابط التنصيب (https://dashboard.heroku.com/new?template=https://github.com/rogerpq/ZQ_LO)[اضغـط هنـا](⌁︙ يرجى متابعة قناة ريبثون الرسمية لتنصيب السورس - @Repthon)\n⌁︙قناة السورس - @Repthon")
+        await event.edit("⌁︙**اهـلا بك فـي تنصيب السورس** \n⌁︙[اضغـط هنـا](https://dashboard.heroku.com/new?template=https://github.com/rogerpq/ZQ_LO)رابط التنصيب(⌁︙ يرجى متابعة قناة ريبثون الرسمية لتنصيب السورس - @Repthon)\n⌁︙قناة السورس - @Repthon")
